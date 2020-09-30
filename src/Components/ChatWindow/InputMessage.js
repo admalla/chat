ad97b97d09e9fd3bfb639ea01fff45dbfb058957
Adam Suskhanov <maasovec@gmail.com>
2020-09-30 12:34:59 +0300

    after Prettier

--- src/Components/ChatWindow/InputMessage.js
+++ src/Components/ChatWindow/InputMessage.js
@@ -4,23 +4,27 @@ import { CSSTransition } from "react-transition-group";
 import { useDispatch, useSelector } from "react-redux";
 import initialAnimation, { backMic } from "../../Redux/Animation/action";
 import "./Style/animation.css";
-import {sentContent, sentMessage, typeMessage} from "../../Redux/ChatWindow/action";
+import {
+  sentContent,
+  sentMessage,
+  typeMessage,
+} from "../../Redux/ChatWindow/action";
 
 function InputMessage() {
   const send = useSelector((state) => state.animation.send);
   const mic = useSelector((state) => state.animation.mic);
-  const contactId = useSelector(state => state.contacts.contact);
-  const content = useSelector(state => state.messages.content);
-  const type = useSelector(state => state.messages.type);
-  const profile = useSelector(state => state.contacts.profile);
+  const contactId = useSelector((state) => state.contacts.contact);
+  const content = useSelector((state) => state.messages.content);
+  const type = useSelector((state) => state.messages.type);
+  const profile = useSelector((state) => state.contacts.profile);
 
   const dispatch = useDispatch();
 
   const handleOnClick = () => {
-    dispatch(sentMessage(profile._id, contactId._id,  content, type));
-  }
+    dispatch(sentMessage(profile._id, contactId._id, content, type));
+  };
 
-  console.log(handleOnClick())
+  console.log(handleOnClick());
 
   const handleChange = (e) => {
     dispatch(sentContent(e.target.value));
@@ -32,23 +36,24 @@ function InputMessage() {
     }
   };
 
-  const handleKeyDown = e => {
-    if(e.keyCode === 13) {
-      dispatch(sentMessage(profile._id, contactId._id,  content, type))
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      dispatch(sentMessage(profile._id, contactId._id, content, type));
     }
-  }
+  };
 
   return (
     <div className="input">
-      <input placeholder="Write a message"
-             type='text'
-             value={content}
-             onChange={handleChange}
-             onKeyDown={() => {
-               if(content.length > 0) {
-                 return handleKeyDown
-               }
-             }}
+      <input
+        placeholder="Write a message"
+        type="text"
+        value={content}
+        onChange={handleChange}
+        onKeyDown={() => {
+          if (content.length > 0) {
+            return handleKeyDown;
+          }
+        }}
       />
 
       <span className="material-icons">attach_file</span>
@@ -63,7 +68,9 @@ function InputMessage() {
         classNames="my-style"
         unmountOnExit
       >
-        <span className="material-icons send-animation" onClick={handleOnClick}>send</span>
+        <span className="material-icons send-animation" onClick={handleOnClick}>
+          send
+        </span>
       </CSSTransition>
     </div>
   );
